24b9251697124f813d76dbd4b29e458015ef1eea
Egor Larionov <egor.larionov@gmail.com>
2018-03-01 13:23:17 -0800

    Add crate documentation and readme.

---
+++ README.md
@@ -0,0 +1,95 @@
+# Ipopt-rs
+
+This crate provides a safe Rust interface to the [Ipopt](https://projects.coin-or.org/Ipopt)
+non-linear optimization library. From the Ipopt webpage:
+
+> Ipopt (**I**nterior **P**oint **OPT**imizer, pronounced eye-pea-Opt) is a software package
+> for large-scale nonlinear optimization. It is designed to find (local) solutions of
+> mathematical optimization problems of the from
+>
+>```verbatim
+>    min     f(x)
+>    x in R^n
+>
+>    s.t.       g_L <= g(x) <= g_U
+>               x_L <=  x   <= x_U
+>```
+>
+> where `f(x): R^n --> R` is the objective function, and `g(x): R^n --> R^m` are the
+> constraint functions. The vectors `g_L` and `g_U` denote the lower and upper bounds
+> on the constraints, and the vectors `x_L` and `x_U` are the bounds on the variables
+> `x`. The functions `f(x)` and `g(x)` can be nonlinear and nonconvex, but should be
+> twice continuously differentiable. Note that equality constraints can be
+> formulated in the above formulation by setting the corresponding components of
+> `g_L` and `g_U` to the same value.
+
+This crate somewhat simplifies the C-interface exposed by Ipopt. Notably it handles the
+boilerplate code required to solve simple unconstrained problems.
+
+# Examples
+
+Solve a simple unconstrained problem using L-BFGS: minimize `(x - 1)^2 + (y -1)^2`
+
+
+```
+extern crate ipopt;
+#[macro_use] extern crate approx; // for floating point equality tests
+
+use ipopt::*;
+
+struct NLP {
+}
+
+impl BasicProblem for NLP {
+    // There are two independent variables: x and y.
+    fn num_variables(&self) -> usize {
+        2
+    }
+    // The variables are unbounded. Any lower bound lower than -10^9 and upper bound higher
+    // than 10^9 is treated effectively as infinity. These absolute infinity limits can be
+    // changed via the `nlp_lower_bound_inf` and `nlp_upper_bound_inf` Ipopt options.
+    fn bounds(&self) -> (Vec<Number>, Vec<Number>) {
+        (vec![-1e20; 2], vec![1e20; 2])
+    }
+
+    // Set the initial conditions for the solver.
+    fn initial_point(&self) -> Vec<Number> {
+        vec![0.0, 0.0]
+    }
+
+    // The objective to be minimized.
+    fn objective(&mut self, x: &[Number], obj: &mut Number) -> bool {
+        *obj = (x[0] - 1.0)*(x[0] - 1.0) + (x[1] - 1.0)*(x[1] - 1.0);
+        true
+    }
+
+    // Objective gradient is used to find a new search direction to find the critical point.
+    fn objective_grad(&mut self, x: &[Number], grad_f: &mut [Number]) -> bool {
+        grad_f[0] = 2.0*(x[0] - 1.0);
+        grad_f[1] = 2.0*(x[1] - 1.0);
+        true
+    }
+}
+
+fn main() {
+    let nlp = NLP { };
+    let mut ipopt = Ipopt::new_unconstrained(nlp);
+
+    // Set Ipopt specific options here a list of all options is available at
+    // https://www.coin-or.org/Ipopt/documentation/node40.html
+    ipopt.set_option("tol", 1e-9); // set error tolerance
+    ipopt.set_option("print_level", 5); // set the print level (5 is the default)
+
+    let (r, obj) = ipopt.solve();
+
+    {
+        let x = ipopt.solution(); // retrieve the solution
+        assert_eq!(r, ReturnStatus::SolveSucceeded);
+        assert_relative_eq!(x[0], 1.0, epsilon = 1e-10);
+        assert_relative_eq!(x[1], 1.0, epsilon = 1e-10);
+        assert_relative_eq!(obj, 0.0, epsilon = 1e-10);
+    }
+}
+```
+
+See the tests for more examples including constrained optimization.
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,113 @@
+#![warn(missing_docs)]
+
+/*!
+ * # Ipopt-rs
+ * 
+ * This crate provides a safe Rust interface to the [Ipopt](https://projects.coin-or.org/Ipopt)
+ * non-linear optimization library. From the Ipopt webpage:
+ *
+ * > Ipopt (**I**nterior **P**oint **OPT**imizer, pronounced eye-pea-Opt) is a software package
+ * > for large-scale nonlinear optimization. It is designed to find (local) solutions of
+ * > mathematical optimization problems of the from
+ * >
+ * >```verbatim
+ * >    min     f(x)
+ * >    x in R^n
+ * >
+ * >    s.t.       g_L <= g(x) <= g_U
+ * >               x_L <=  x   <= x_U
+ * >```
+ * >
+ * > where `f(x): R^n --> R` is the objective function, and `g(x): R^n --> R^m` are the
+ * > constraint functions. The vectors `g_L` and `g_U` denote the lower and upper bounds
+ * > on the constraints, and the vectors `x_L` and `x_U` are the bounds on the variables
+ * > `x`. The functions `f(x)` and `g(x)` can be nonlinear and nonconvex, but should be
+ * > twice continuously differentiable. Note that equality constraints can be
+ * > formulated in the above formulation by setting the corresponding components of
+ * > `g_L` and `g_U` to the same value. 
+ *
+ * This crate somewhat simplifies the C-interface exposed by Ipopt. Notably it handles the
+ * boilerplate code required to solve simple unconstrained problems.
+ *
+ * # Examples
+ *
+ * Solve a simple unconstrained problem using L-BFGS: minimize `(x - 1)^2 + (y -1)^2`
+ *
+ *
+ * ```
+ * extern crate ipopt;
+ * #[macro_use] extern crate approx; // for floating point equality tests
+ * 
+ * use ipopt::*;
+ * 
+ * struct NLP {
+ * }
+ * 
+ * impl BasicProblem for NLP {
+ *     // There are two independent variables: x and y.
+ *     fn num_variables(&self) -> usize {
+ *         2
+ *     }    
+ *     // The variables are unbounded. Any lower bound lower than -10^9 and upper bound higher
+ *     // than 10^9 is treated effectively as infinity. These absolute infinity limits can be
+ *     // changed via the `nlp_lower_bound_inf` and `nlp_upper_bound_inf` Ipopt options.
+ *     fn bounds(&self) -> (Vec<Number>, Vec<Number>) {
+ *         (vec![-1e20; 2], vec![1e20; 2])
+ *     }
+ *
+ *     // Set the initial conditions for the solver.
+ *     fn initial_point(&self) -> Vec<Number> {
+ *         vec![0.0, 0.0]
+ *     }
+ *
+ *     // The objective to be minimized.
+ *     fn objective(&mut self, x: &[Number], obj: &mut Number) -> bool {
+ *         *obj = (x[0] - 1.0)*(x[0] - 1.0) + (x[1] - 1.0)*(x[1] - 1.0);
+ *         true
+ *     }
+ *
+ *     // Objective gradient is used to find a new search direction to find the critical point.
+ *     fn objective_grad(&mut self, x: &[Number], grad_f: &mut [Number]) -> bool {
+ *         grad_f[0] = 2.0*(x[0] - 1.0);
+ *         grad_f[1] = 2.0*(x[1] - 1.0);
+ *         true
+ *     }
+ * }
+ * 
+ * fn main() {
+ *     let nlp = NLP { };
+ *     let mut ipopt = Ipopt::new_unconstrained(nlp);
+ *
+ *     // Set Ipopt specific options here a list of all options is available at
+ *     // https://www.coin-or.org/Ipopt/documentation/node40.html
+ *     ipopt.set_option("tol", 1e-9); // set error tolerance
+ *     ipopt.set_option("print_level", 5); // set the print level (5 is the default)
+ *
+ *     let (r, obj) = ipopt.solve();
+ *
+ *     {
+ *         let x = ipopt.solution(); // retrieve the solution
+ *         assert_eq!(r, ReturnStatus::SolveSucceeded);
+ *         assert_relative_eq!(x[0], 1.0, epsilon = 1e-10);
+ *         assert_relative_eq!(x[1], 1.0, epsilon = 1e-10);
+ *         assert_relative_eq!(obj, 0.0, epsilon = 1e-10);
+ *     }
+ * }
+ * ```
+ *
+ * See the tests for more examples including constrained optimization.
+ *
+ */
+
 extern crate ipopt_sys as ffi;
 
 use ffi::{Bool, Int};
 use std::ffi::CString;
 use std::slice;
 
+/// Uniform floating point number type.
 pub type Number = f64; // Same as ffi::Number
+/// Index type used to access internal buffers.
 pub type Index = i32;  // Same as ffi::Index
 
 /// The non-linear problem to be solved by Ipopt. This trait specifies all the
@@ -643,32 +746,131 @@ impl<P: BasicProblem> Drop for Ipopt<P> {
 /// Zero-based indexing (C Style) or one-based indexing (Fortran style).
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum IndexingStyle {
+    /// C-style array indexing starting from 0.
     CStyle = 0,
+    /// Fortran-style array indexing starting from 1.
     FortranStyle = 1,
 }
 
 /// Program return status.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum ReturnStatus {
+    /// Console Message: `EXIT: Optimal Solution Found.`
+    ///
+    /// This message indicates that IPOPT found a (locally) optimal point within the desired tolerances.
     SolveSucceeded,
+    /// Console Message: `EXIT: Solved To Acceptable Level.`
+    ///
+    /// This indicates that the algorithm did not converge to the "desired" tolerances, but that
+    /// it was able to obtain a point satisfying the "acceptable" tolerance level as specified by
+    /// the [`acceptable_*`
+    /// ](https://www.coin-or.org/Ipopt/documentation/node42.html#opt:acceptable_tol) options. This
+    /// may happen if the desired tolerances are too small for the current problem. 
     SolvedToAcceptableLevel,
-    InfeasibleProblemDetected,
-    SearchDirectionBecomesTooSmall,
-    DivergingIterates,
-    UserRequestedStop,
+    /// Console Message: `EXIT: Feasible point for square problem found.`
+    ///
+    /// This message is printed if the problem is "square" (i.e., it has as many equality
+    /// constraints as free variables) and IPOPT found a feasible point. 
     FeasiblePointFound,
+    /// Console Message: `EXIT: Converged to a point of local infeasibility. Problem may be
+    /// infeasible.`
+    /// 
+    /// The restoration phase converged to a point that is a minimizer for the constraint violation
+    /// (in the l1-norm), but is not feasible for the original problem. This indicates that
+    /// the problem may be infeasible (or at least that the algorithm is stuck at a locally
+    /// infeasible point). The returned point (the minimizer of the constraint violation) might
+    /// help you to find which constraint is causing the problem. If you believe that the NLP is
+    /// feasible, it might help to start the optimization from a different point. 
+    InfeasibleProblemDetected,
+    /// Console Message: `EXIT: Search Direction is becoming Too Small.`
+    ///
+    /// This indicates that IPOPT is calculating very small step sizes and is making very little
+    /// progress. This could happen if the problem has been solved to the best numerical accuracy
+    /// possible given the current scaling.
+    SearchDirectionBecomesTooSmall,
+    /// Console Message: `EXIT: Iterates diverging; problem might be unbounded.`
+    /// 
+    /// This message is printed if the max-norm of the iterates becomes larger than the value of
+    /// the option [`diverging_iterates_tol`
+    /// ](https://www.coin-or.org/Ipopt/documentation/node42.html#opt:diverging_iterates_tol).
+    /// This can happen if the problem is unbounded below and the iterates are diverging.
+    DivergingIterates,
+    /// Console Message: `EXIT: Stopping optimization at current point as requested by user.`
+    ///
+    /// This message is printed if the user call-back method intermediate_callback returned false
+    /// (see Section [3.3.4](https://www.coin-or.org/Ipopt/documentation/node23.html#sec:add_meth)).
+    UserRequestedStop,
+    /// Console Message: `EXIT: Maximum Number of Iterations Exceeded.`
+    ///
+    /// This indicates that IPOPT has exceeded the maximum number of iterations as specified by the
+    /// option [`max_iter`](https://www.coin-or.org/Ipopt/documentation/node42.html#opt:max_iter).
     MaximumIterationsExceeded,
-    RestorationFailed,
-    ErrorInStepComputation,
+    /// Console Message: `EXIT: Maximum CPU time exceeded.`
+    ///
+    /// This indicates that IPOPT has exceeded the maximum number of CPU seconds as specified by
+    /// the option
+    /// [`max_cpu_time`](https://www.coin-or.org/Ipopt/documentation/node42.html#opt:max_cpu_time).
     MaximumCpuTimeExceeded,
-    NotEnoughDegreesOfFreedom,
-    InvalidProblemDefinition,
+    /// Console Message: `EXIT: Restoration Failed!`
+    ///
+    /// This indicates that the restoration phase failed to find a feasible point that was
+    /// acceptable to the filter line search for the original problem. This could happen if the
+    /// problem is highly degenerate, does not satisfy the constraint qualification, or if your NLP
+    /// code provides incorrect derivative information. 
+    RestorationFailed,
+    /// Console Output: `EXIT: Error in step computation (regularization becomes too large?)!`
+    ///
+    /// This messages is printed if IPOPT is unable to compute a search direction, despite several
+    /// attempts to modify the iteration matrix. Usually, the value of the regularization parameter
+    /// then becomes too large. One situation where this can happen is when values in the Hessian
+    /// are invalid (NaN or Inf). You can check whether this is true by using the
+    /// [`check_derivatives_for_naninf`
+    /// ](https://www.coin-or.org/Ipopt/documentation/node44.html#opt:check_derivatives_for_naninf)
+    /// option.
+    ErrorInStepComputation,
+    /// Console Message: (details about the particular error will be output to the console)
+    ///
+    /// This indicates that there was some problem specifying the options. See the specific message
+    /// for details.
     InvalidOption,
+    /// Console Message: `EXIT: Problem has too few degrees of freedom.`
+    ///
+    /// This indicates that your problem, as specified, has too few degrees of freedom. This can
+    /// happen if you have too many equality constraints, or if you fix too many variables (IPOPT
+    /// removes fixed variables by default, see also the [`fixed_variable_treatment`
+    /// ](https://www.coin-or.org/Ipopt/documentation/node44.html#opt:fixed_variable_treatment)
+    /// option).
+    NotEnoughDegreesOfFreedom,
+    /// Console Message: (no console message, this is a return code for the C and Fortran
+    /// interfaces only.)
+    ///
+    /// This indicates that there was an exception of some sort when building the IpoptProblem
+    /// structure in the C or Fortran interface. Likely there is an error in your model or the main
+    /// routine.
+    InvalidProblemDefinition,
+    /// An invalid number like `NaN` was detected.
     InvalidNumberDetected,
+    /// Console Message: (details about the particular error will be output to the console)
+    ///
+    /// This indicates that IPOPT has thrown an exception that does not have an internal return
+    /// code. See the specific message for details.
     UnrecoverableException,
+    /// Console Message: `Unknown Exception caught in Ipopt`
+    ///
+    /// An unknown exception was caught in IPOPT. This exception could have originated from your
+    /// model or any linked in third party code.
     NonIpoptExceptionThrown,
+    /// Console Message: `EXIT: Not enough memory.`
+    ///
+    /// An error occurred while trying to allocate memory. The problem may be too large for your
+    /// current memory and swap configuration. 
     InsufficientMemory,
+    /// Console: `EXIT: INTERNAL ERROR: Unknown SolverReturn value - Notify IPOPT Authors.`
+    ///
+    /// An unknown internal error has occurred. Please notify the authors of IPOPT via the mailing
+    /// list.
     InternalError,
+    /// Unclassified error.
     UnknownError,
 }
 
