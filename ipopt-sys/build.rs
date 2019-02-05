eb16c4ae62f4d4530df09bc341a8c5ab90e060ee
Egor Larionov <egor.larionov@gmail.com>
2019-02-04 22:40:18 -0800

    Get sys crate to compile on linux

--- ipopt-sys/build.rs
+++ ipopt-sys/build.rs
@@ -48,26 +48,44 @@ use std::process::Command;
 use std::{env, fs};
 use tar::Archive;
 
-const LIBRARY: &'static str = "ipopt";
-const SOURCE_URL: &'static str = "https://www.coin-or.org/download/source/Ipopt";
-const VERSION: &'static str = "3.12.10";
-const MIN_VERSION: &'static str = "3.12.8";
+const LIBRARY: &str = "ipopt";
+const SOURCE_URL: &str = "https://www.coin-or.org/download/source/Ipopt";
+const VERSION: &str = "3.12.10";
+const MIN_VERSION: &str = "3.12.8";
 const BINARY_DL_URL: &str = "https://github.com/JuliaOpt/IpoptBuilder/releases/download/";
 
 #[cfg(target_os = "macos")]
-static LIB_EXT: &'static str = "dylib";
-#[cfg(target_os = "macos")]
-static BINARY_SUFFIX: &'static str = "x86_64-apple-darwin14.tar.gz";
+mod platform {
+    pub static LIB_EXT: &str = "dylib";
+    pub static BINARY_SUFFIX: &str = "x86_64-apple-darwin14.tar.gz";
+}
 
 #[cfg(target_os = "linux")]
-static LIB_EXT: &'static str = "so";
-#[cfg(target_os = "linux")]
-static BINARY_SUFFIX: &'static str = "x86_64-linux-gnu-gcc8.tar.gz";
+mod platform {
+    pub static LIB_EXT: &str = "so";
+    pub static BINARY_SUFFIX: &str = "x86_64-linux-gnu-gcc8.tar.gz";
+    pub static BINARY_MD5: &str = "9c406cb1b54918b56945548e64b8e9ca";
+    pub static BINARY_SHA1: &str = "a940b1f70021ddbd057643a056b61228d68f26e6";
+}
+
+#[cfg(target_family = "unix")]
+mod family {
+    pub static LIB_MAJ_VER: &str = "1";
+    pub static LIB_MIN_VER: &str = "10.10";
+}
 
 #[cfg(target_os = "windows")]
-static LIB_EXT: &'static str = "dll";
+mod platform {
+    pub static LIB_EXT: &str = "dll";
+    pub static BINARY_SUFFIX: &str = "x86_64-w64-mingw32-gcc8.tar.gz";
+}
+
 #[cfg(target_os = "windows")]
-static BINARY_SUFFIX: &'static str = "x86_64-w64-mingw32-gcc8.tar.gz";
+mod family {
+}
+
+use crate::platform::*;
+use crate::family::*;
 
 lazy_static! {
     static ref BINARY_NAME: String = format!(
@@ -93,22 +111,39 @@ fn main() {
         unimplemented!();
     }
 
-    if let Ok(ipopt_install_path) = build_and_install_with_mkl() {
-        link(build_cnlp(ipopt_install_path)).unwrap();
-        return;
+    let mut msg = String::from("\n\n");
+
+    match build_and_install_ipopt() {
+        Ok(ipopt_install_path) => {
+            link(build_cnlp(ipopt_install_path)).expect("Failed to create bindings for Ipopt library.");;
+            return;
+        }
+        Err(err) => {
+            msg.push_str(&format!("Failed to build Ipopt from source: {:?}\n\n", err));
+        }
     }
 
-    if let Ok(_) = download_and_install_prebuilt_binary() {
-        return;
+    match download_and_install_prebuilt_binary() {
+        Ok(ipopt_install_path) => {
+            link(build_cnlp(ipopt_install_path)).expect("Failed to create bindings for Ipopt library.");
+            return;
+        }
+        Err(err) => {
+            msg.push_str(&format!("Failed to download and install Ipopt binaries: {:?}\n\n", err));
+        }
     }
+
+    panic!(msg);
 }
 
 #[derive(Clone, Debug, PartialEq)]
 enum Error {
     MKLInstallNotFound,
     DownloadFailure { response_code: u32, url: String },
+    UrlFailure,
     UnsupportedPlatform,
     IOError,
+    HashMismatch,
 }
 
 impl From<std::io::Error> for Error {
@@ -117,8 +152,42 @@ impl From<std::io::Error> for Error {
     }
 }
 
+impl From<curl::Error> for Error {
+    fn from(_: curl::Error) -> Error {
+        Error::UrlFailure
+    }
+}
+
+fn library_name() -> String {
+    format!("lib{}.{}", LIBRARY, LIB_EXT)
+}
+
+#[cfg(target_family = "windows")]
+fn versioned_library_name() -> String {
+    // No versioning in filenames on Windows.
+    format!("lib{}.{}", LIBRARY, LIB_EXT)
+}
+
+#[cfg(target_family = "unix")]
+fn versioned_library_name() -> String {
+    if cfg!(target_os = "macos") {
+        format!("lib{}.{}.{}.{}", LIBRARY, LIB_MAJ_VER, LIB_MIN_VER, LIB_EXT)
+    } else {
+        format!("lib{}.{}.{}.{}", LIBRARY, LIB_EXT, LIB_MAJ_VER, LIB_MIN_VER)
+    }
+}
+
+#[cfg(target_family = "unix")]
+fn major_versioned_library_name() -> String {
+    if cfg!(target_os = "macos") {
+        format!("lib{}.{}.{}", LIBRARY, LIB_MAJ_VER, LIB_EXT)
+    } else {
+        format!("lib{}.{}.{}", LIBRARY, LIB_EXT, LIB_MAJ_VER)
+    }
+}
+
 /// Download the ipopt prebuilt binary from JuliaOpt and install it.
-fn download_and_install_prebuilt_binary() -> Result<(), Error> {
+fn download_and_install_prebuilt_binary() -> Result<PathBuf, Error> {
     let file_name = BINARY_NAME.clone();
 
     // Extract the filename from the URL
@@ -144,26 +213,46 @@ fn download_and_install_prebuilt_binary() -> Result<(), Error> {
     let unpacked_dir = download_dir.join(base_name);
     let output = PathBuf::from(&env::var("OUT_DIR").unwrap());
     let install_dir = output.clone();
-    let library_file = format!("lib{}.{}", LIBRARY, LIB_EXT);
-    let library_path = install_dir.join("lib").join(&library_file);
+    let library_file = versioned_library_name();
+    let lib_dir = install_dir.join("lib");
+    let library_path = lib_dir.join(&library_file);
     if library_path.exists() {
         println!("File {} already exists, deleting.", library_path.display());
         fs::remove_file(&library_path).unwrap();
+    }
+
+    // On unix make sure all artifacts are removed to cleanup the environment
+    if cfg!(target_family = "unix") {
+        fs::remove_file(lib_dir.join(major_versioned_library_name())).ok();
+        fs::remove_file(lib_dir.join(library_name())).ok();
     }
 
     // Build destination path
     let tarball_path = download_dir.join(file_name);
     dbg!(&tarball_path);
 
-    download_tarball(&tarball_path, &BINARY_URL)?;
-    extract_tarball(tarball_path, &download_dir);
+    if !unpacked_dir.exists() {
+        download_tarball(&tarball_path, &BINARY_URL)?;
+        check_tarball_hashes(&tarball_path)?;
+        extract_tarball(tarball_path, &unpacked_dir);
+    }
 
     // Copy lib
+    if !lib_dir.exists() {
+        fs::create_dir(&lib_dir)?;
+    }
     fs::copy(
         unpacked_dir.join("lib").join(&library_file),
-        install_dir.join("lib").join(&library_file),
+        &library_path,
     )
     .unwrap();
+
+    // Make links (on unix only)
+    if cfg!(target_family = "unix") {
+        use std::os::unix::fs::symlink;
+        symlink(&library_path, lib_dir.join(major_versioned_library_name()))?;
+        symlink(&library_path, lib_dir.join(library_name()))?;
+    }
 
     // Copy headers
     let install_include_dir = install_dir.join("include").join("coin");
@@ -175,6 +264,36 @@ fn download_and_install_prebuilt_binary() -> Result<(), Error> {
     for entry in fs::read_dir(include_dir)? {
         let file = entry?;
         fs::copy(file.path(), install_include_dir.join(file.file_name())).unwrap();
+    }
+
+    Ok(install_dir)
+}
+
+fn check_tarball_hashes(tarball_path: &Path) -> Result<(), Error> {
+    use std::io::Read;
+    use crypto::digest::Digest;
+
+    {
+        let mut f = File::open(tarball_path)?;
+        let mut buffer = Vec::new();
+        f.read_to_end(&mut buffer)?;
+        let mut hasher = crypto::md5::Md5::new();
+        hasher.input(&buffer);
+        let dl_hex = hasher.result_str();
+        if BINARY_MD5 != dl_hex {
+            return Err(Error::HashMismatch);
+        }
+    }
+    {
+        let mut f = File::open(tarball_path)?;
+        let mut buffer = Vec::new();
+        f.read_to_end(&mut buffer)?;
+        let mut hasher = crypto::sha1::Sha1::new();
+        hasher.input(&buffer);
+        let dl_hex = hasher.result_str();
+        if BINARY_SHA1 != dl_hex {
+            return Err(Error::HashMismatch);
+        }
     }
 
     Ok(())
@@ -216,6 +335,7 @@ fn download_tarball(tarball_path: &Path, binary_url: &str) -> Result<(), Error> 
         let f = File::create(tarball_path).unwrap();
         let mut writer = BufWriter::new(f);
         let mut easy = Easy::new();
+        easy.follow_location(true)?;
         easy.url(binary_url).unwrap();
         easy.write_function(move |data| Ok(writer.write(data).unwrap()))
             .unwrap();
@@ -234,7 +354,7 @@ fn download_tarball(tarball_path: &Path, binary_url: &str) -> Result<(), Error> 
 }
 
 /// Build Ipopt statically linked to MKL if possible. Return the path to the ipopt library.
-fn build_and_install_with_mkl() -> Result<PathBuf, Error> {
+fn build_and_install_ipopt() -> Result<PathBuf, Error> {
     // Compile ipopt from source
     // Build URL to download from
     let binary_url = format!("{}/Ipopt-{}.tgz", SOURCE_URL, VERSION);
@@ -296,6 +416,18 @@ fn build_and_install_with_mkl() -> Result<PathBuf, Error> {
     let proj_root_dir = env::current_dir().unwrap();
     env::set_current_dir(build_dir).unwrap();
 
+    let res = build_ipopt(&install_dir, debug);
+
+    // Restore current directory
+    env::set_current_dir(proj_root_dir).unwrap();
+
+    res?; // Propagate any errors after we have restored the current dir.
+
+    Ok(install_dir)
+}
+
+// Build Ipopt with MKL in the current directory.
+fn build_ipopt(install_dir: &Path, debug: bool) -> Result<(), Error> {
     // Look for intel MKL and link to its libraries if found.
     let mkl_root = PathBuf::from(env::var("MKLROOT").unwrap_or("/opt/intel/mkl".to_string()));
     dbg!(&mkl_root);
@@ -322,7 +454,7 @@ fn build_and_install_with_mkl() -> Result<PathBuf, Error> {
         }
     };
 
-    run(unpacked_dir.join("configure").to_str().unwrap(), |cmd| {
+    run(env::current_dir()?.join("configure").to_str().unwrap(), |cmd| {
         let cmd = cmd
             .arg(format!("--prefix={}", install_dir.display()))
             .arg("--enable-shared")
@@ -339,11 +471,7 @@ fn build_and_install_with_mkl() -> Result<PathBuf, Error> {
     run("make", |cmd| cmd.arg(format!("-j{}", num_cpus::get())));
     //run("make", |cmd| cmd.arg("test")); // Ensure everything is working
     run("make", |cmd| cmd.arg("install")); // Install to install_dir
-
-    // Restore current directory
-    env::set_current_dir(proj_root_dir).unwrap();
-
-    Ok(install_dir)
+    Ok(())
 }
 
 fn remove_suffix(value: &mut String, suffix: &str) {
@@ -353,7 +481,8 @@ fn remove_suffix(value: &mut String, suffix: &str) {
     }
 }
 
-fn extract_tarball<P: AsRef<Path>, P2: AsRef<Path>>(archive_path: P, extract_to: P2) {
+fn extract_tarball<P: AsRef<Path> + std::fmt::Debug, P2: AsRef<Path> + std::fmt::Debug>(archive_path: P, extract_to: P2) {
+    println!("Extracting tarball {:?} to {:?}", &archive_path, &extract_to);
     let file = File::open(archive_path).unwrap();
     let mut a = Archive::new(GzDecoder::new(file));
     a.unpack(extract_to).unwrap();
